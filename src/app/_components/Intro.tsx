631c79de6f43e4d78ff181bf2f8e00f7d3ed87c1
25LP4412 25LP4412 <25LP4412@pinecones-iMac.local>
2025-12-08 12:29:56 +0800

    remove images

---
+++ src/app/_components/Intro.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Command, CommandInput } from "@/components/ui/command";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Badge, ChevronDown, ChevronRight } from "lucide-react";
+import { Genres } from "./Genres";
+import { useEffect, useState } from "react";
+import { SearchSection } from "./SearchSection";
+import { ModeToggle } from "./ModeToggle";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useTheme } from "next-themes";
+import GenreList from "./GenreList";
+
+export type Genre = {
+  id: number;
+  name: string;
+};
+type Response = {
+  genres: Genre[];
+};
+
+export const Intro = () => {
+  const [genres, setgenres] = useState<Genre[]>([]);
+  const { setTheme, theme } = useTheme();
+
+  const searchParams = useSearchParams();
+  const genreIds = searchParams.get("genreIds")?.split(",") || [];
+  console.log("aaaaA", genreIds);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleClickGenre = (genreId: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    const updatedGenreIds = genreIds?.includes(genreId)
+      ? genreIds.filter((id) => id !== genreId)
+      : [...genreIds, genreId];
+    params.set("genreIds", updatedGenreIds.join(","));
+    router.push("/genresfilter" + "?" + params);
+  };
+
+  useEffect(() => {
+    const getGenre = async () => {
+      try {
+        const res = await fetch(
+          "https://api.themoviedb.org/3/genre/movie/list?language=en",
+          {
+            method: "GET",
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGEzNzg5MTlmNDZjZjgwYmNhNDZkMThiYTY2NzQ0MiIsIm5iZiI6MTc2MzUyMzY0Mi43NTEwMDAyLCJzdWIiOiI2OTFkM2MzYTYyYTA5ZGE0NmQ3YWQ2ZDYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.HZMwjz4_eYyA0XA28jMAQt2UFvsMXnmYm0DFdEFLGMk",
+              accept: "application/json",
+            },
+          }
+        );
+        const data = (await res.json()) as Response;
+        setgenres(data.genres);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getGenre();
+  }, []);
+  return (
+    <div className="h-9 w-screen flex justify-center  text-center items-center my-[11.5px] px-20">
+      <img alt="logo" src="/Logo.png" className="w-[92px] h-5 mr-[332px]" />
+      <Popover>
+        <PopoverTrigger className="flex" asChild>
+          <Button variant="outline">
+            <ChevronDown />
+            Genre
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-144.25 ">
+          <GenreList path="genres" />
+        </PopoverContent>
+      </Popover>
+      <SearchSection />
+      <ModeToggle />
+    </div>
+  );
+};
